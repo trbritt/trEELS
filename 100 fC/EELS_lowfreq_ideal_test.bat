9f3bc5874d7e91da3f896a3c87ed082cf7b6c83e
Tristan Britt <tristan.britt@mail.mcgill.ca>
2020-07-09 14:42:35 -0400

    Updated to match initial transverse emittance - obtained EELS signal

--- 100 fC/EELS_lowfreq_ideal_test.bat
+++ 100 fC/EELS_lowfreq_ideal_test.bat
@@ -3,11 +3,12 @@ REM gdf2a -w 16 -v -o sol_ideal_test.txt sol_ideal_test.gdf
 REM phi110=1.27
 
 mr -o inputpulse_ideal.gdf EELS.mr gpt UED_ideal.in outputT=0 outputZ=0.50 N=10000 div=8e5
-mr -o EELSwithSample_lowfreq_ideal_test_phi110.gdf EELS_test.mr gpt EELS_ideal.in E0=2.9e6 phi=0.66 Isol2=500.08 Isol3=921.00 scale=20 N=10000 B0=3e-3 outputZ=2.2 zsample=0.53 sc=1 streak=1 div=8e5
-REM gdfa -o EELSwithSampleStds_lowfreq_ideal_test_Isol3.gdf EELSwithSample_lowfreq_ideal_test_Isol3.gdf time avgz stdx stdz avgBz avgBx avgG nemixrms nemizrms stdG
+mr -o EELSwithSample_lowfreq_ideal_test_phi110.gdf EELS_test.mr gpt EELS_ideal.in E0=2.929e6 phi=0.65 Isol2=575.8 Isol3=924.00 scale=20 N=10000 B0=3e-3 outputZ=2.2 zsample=0.53 sc=1 streak=1 div=8e5
+REM gdfa -o EELSwithSampleStds_lowfreq_ideal_test_phi110.gdf EELSwithSample_lowfreq_ideal_test_phi110.gdf time avgz stdx stdz avgBz avgBx avgG nemixrms nemizrms stdG
 
 
 REM mr -o tmp.gdf EELS.mr gpt EELS_ideal.in E0=2.9e6 phi=0.66 Isol2=500.08 Isol3=921.00 phi110=1.25 B0=3e-3 scale=20 N=10000 zsample=0.53 sc=1 streak=1 div=8e5 outputT=0
 REM gdfsolve -v -o sol_test.gdf solveEELSonDetector_lowfreq_ideal.sol mr EELS.mr gpt EELS_ideal_streak_test.in N=10000 B0=3e-3 outputT=0 outputZ=2.2 streak=1 div=8e5
 REM gdf2a -w 16 -v -o sol_test.txt sol_test.gdf
+
 REM mr -o tmp_streak.gdf EELS_test.mr gpt EELS_ideal_streak_test.in N=10000 B0=3e-3 outputT=0 outputZ=2.2 streak=1 div=8e5 Isol3=915.00
--- 100 fC/solveEELSonDetector_lowfreq_ideal.sol
+++ 100 fC/solveEELSonDetector_lowfreq_ideal.sol
@@ -3,24 +3,26 @@ backtracking = 0 ;
 forcejacobian = 1 ;
 
 [VARIABLES]
-Isol2=600;
-Isol2.reldelta = 0.01;
-Isol2.min=600;
+
+Isol3=924;
+Isol3.reldelta=0.01;
+
+phi110=1.16;
+phi110.reldelta = 0.01;
 
 phi=0.65;
 phi.reldelta = 0.01;
 phi.min = -1.5;
 phi.max = 1.5;
 
-E0 = 2.93e6;
+E0 = 2.929e6;
 E0.reldelta = 0.01;
 E0.min = 0;
 
-Isol3=915;
-Isol3.reldelta=0.01;
+Isol2=570;
+Isol2.reldelta = 0.01;
+Isol2.min=550;
 
-phi110=1.16;
-phi110.reldelta = 0.01;
 
 [CONSTRAINTS]
 
@@ -28,12 +30,11 @@ avgx = 0;
 avgx.position = 2.2;
 avgx.abstol = 100e-6;
 
+stdt = 60e-15;
+stdt.position=1.6;
+stdt.abstol=10e-15;
 
 [OPTIMIZE]
 stdx = 0;
 stdx.position = 2.2;
 stdx.abstol=1e-5;
-
-stdt = 60e-15;
-stdt.position=1.6;
-stdt.abstol=10e-15;
